cde566ed7d689e75e99a89dee0a9a5fd27ba7cd6
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-08-21 23:56:46 +0800

    Added the user model

---
+++ BackEnd/src/main/java/com/example/demo/Model/User.java
@@ -0,0 +1,56 @@
+package com.example.demo.Model;
+
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name= "users")
+public class User implements UserDetails{
+    
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private int id;
+
+    @NotEmpty(message= "You should have a name")
+    private String name;
+
+    @Email(message= "It must be an email")
+    private String username;
+
+    @NotNull(message= "You must have a password")
+    private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+}

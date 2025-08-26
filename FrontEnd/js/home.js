6fc77a7891902c925c21ff862f55722735ab5ccb
Raphae San Juan <raphaelsanjuan6@gmail.com>
2025-08-26 14:19:38 +0800

    Try to commit to fix the backend if possible

--- FrontEnd/js/home.js
+++ FrontEnd/js/home.js
@@ -1,11 +1,15 @@
 const productsContainer = document.getElementsByClassName("products-container")[0];
+const shopsContainer = document.getElementsByClassName("shops-container")[0];
 
 let array = localStorage.getItem("products") ? JSON.parse(localStorage.getItem("products")) : [];
+let shops = localStorage.getItem("shops") ? JSON.parse(localStorage.getItem("shops")) : [];
 
 window.addEventListener("DOMContentLoaded", getProducts());
 
 async function getProducts() {
     const token = JSON.parse(localStorage.getItem("token"));
+
+    console.log(token.token);
     
     if(token != null) {
     const response = await fetch("https://full-stack-dropshipping-trendora.onrender.com/api/trendora/products", {
@@ -15,11 +19,18 @@ async function getProducts() {
         }
     });
 
-    const data = await response.json();
+    const shops = await fetch("https://full-stack-dropshipping-trendora.onrender.com/api/trendora/shops", {
+        method: "GET",
+        headers: {
+            "Authorization": `Bearer ${token.token}`
+        }
+    });
 
-    console.log(data);
+    const data = await response.json();
+    const partners = await shops.json();
 
     localStorage.setItem("products", JSON.stringify(data));
+    localStorage.setItem("shops", JSON.stringify(partners));
   }
 
   for(const products of array){
@@ -58,4 +69,27 @@ async function getProducts() {
         productsContainer.appendChild(shopProduct);
   }
 
+  for(const shops of shops){
+        const shopPartner = document.createElement("div");
+        shopProduct.classList.add("product-card");
+
+        const image = document.createElement("img");
+        image.src = shops.image;
+        image.alt = shops.name;
+
+        const name = document.createElement("h3");
+        name.textContent = shops.name;
+
+        const button = document.createElement("button");
+        button.textContent = "Follow Shop";
+  
+        shopProduct.append(
+            image,
+            name,
+            button
+        );
+
+        shopsContainer.appendChild(shopPartner);
+
+  }
 }
